80c4d2ce4bff17c4d733919fa84b49423e9ff70b
Armando <armando567@hotmail.com>
2019-01-24 13:04:22 -0600

    Index High Marketing

---
+++ index.php
@@ -0,0 +1,233 @@
+<?php 
+    include('sections/header.php');
+?>
+
+
+<div role="main" class="main">
+            <div class="slider-container rev_slider_wrapper" style="height: 530px;"></div>
+            <div class="home-intro home-intro-quaternary" id="home-intro">
+                <div class="container">
+                    <div class="row text-center">
+                        <div class="col">
+                            <p class="mb-0">
+                                The fastest way to grow your business with the leader in <span class="highlighted-word highlighted-word-animation-1 highlighted-word-animation-1-light text-color-light font-weight-semibold text-5">Technology</span>
+                                <span>Check out our options and features included.</span>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            
+            <div class="container">
+                <div class="row align-items-center mb-5">
+                    <div class="col-lg-7 pr-5 " data-="fadeInRightShorter">
+                        <h2 class="font-weight-normal text-6 mb-4"><strong class="font-weight-extra-bold">Who </strong>We Are</h2>
+                        <p class="lead pr-2 mb-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus blandit massa enikklam id valorem ipsum dolor sit amet, consectetur adipiscinLorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                        <p class="mb-4">Phasellus blandit massa enim. Nullam id varius elit. blandit massa enim  d varius blandit massa enimariusi d varius elit. blandit massa enimariud varius elit. blandit massa enimariusisi ariusius.</p>
+                        <a class="btn btn-light text-uppercase text-primary text-1 py-2 px-4 mb-1" href="#"><strong>VIEW MORE</strong><i class="fas fa-chevron-right text-2 pl-2"></i></a>
+                    </div>
+                    <div class="col-lg-5">
+                        <div class="row mt-5 mt-lg-0">
+                            <div class="col-md-8 col-lg-6 text-center text-lg-left mx-auto">
+                                <img class="img-fluid m-3 my-0 mt-lg-5 pt-lg-5 " src="img/office/our-office-4.jpg" alt="Office" data-="" data--delay="200">
+                            </div>
+                            <div class="col-md-8 col-lg-6 pl-lg-0 text-center text-lg-left mx-auto">
+                                <img class="img-fluid m-3 my-0 " src="img/office/our-office-5.jpg" alt="Office" data-="" data--delay="400">
+                                <img class="img-fluid m-3 my-0 " src="img/office/our-office-7.jpg" alt="Office" data-="" data--delay="200">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <!-- Nuestros servicios -->
+
+            <section class="section bg-color-grey-scale-1 section-height-3 section-no-border " data-="fadeIn">
+                <div class="container">
+                    <div class="row">
+                        <div class="col text-center " data-="fadeInUpShorter" data--delay="200">
+                            <h2 class="font-weight-normal text-6 mb-5">Our <strong class="font-weight-extra-bold">Services</strong></h2>
+                        </div>
+                    </div>
+
+                    <div class="row">
+                        <div class="col-lg-4 mb-4 " data-="fadeInLeftShorter" data--delay="400">
+                            <div class="feature-box feature-box-secondary feature-box-style-4">
+                                <div class="feature-box-icon">
+                                    <i class="icon-user-following icons text-color-primary"></i>
+                                </div>
+                                <div class="feature-box-info">
+                                    <h4 class="mb-2 font-weight-bold">Customer Support</h4>
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing metus elit. Quisque rutrum pellentesque imperdiet.</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-4 mb-4 " data-="fadeIn" data--delay="200">
+                            <div class="feature-box feature-box-secondary feature-box-style-4">
+                                <div class="feature-box-icon">
+                                    <i class="icon-layers icons text-color-primary"></i>
+                                </div>
+                                <div class="feature-box-info">
+                                    <h4 class="mb-2 font-weight-bold">Sliders</h4>
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing metus elit. Quisque rutrum pellentesque imperdiet.</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-4 mb-4 " data-="fadeInRightShorter" data--delay="400">
+                            <div class="feature-box feature-box-secondary feature-box-style-4">
+                                <div class="feature-box-icon">
+                                    <i class="icon-calculator icons text-color-primary"></i>
+                                </div>
+                                <div class="feature-box-info">
+                                    <h4 class="mb-2 font-weight-bold">HTML5</h4>
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing metus elit. Quisque rutrum pellentesque imperdiet.</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="row mt-lg-3">
+                        <div class="col-lg-4 mb-4 mb-lg-0 " data-="fadeInLeftShorter" data--delay="400">
+                            <div class="feature-box feature-box-secondary feature-box-style-4">
+                                <div class="feature-box-icon">
+                                    <i class="icon-star icons text-color-primary"></i>
+                                </div>
+                                <div class="feature-box-info">
+                                    <h4 class="mb-2 font-weight-bold">Icons</h4>
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing metus elit. Quisque rutrum pellentesque imperdiet.</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-4 mb-4 mb-lg-0 " data-="fadeIn" data--delay="200">
+                            <div class="feature-box feature-box-secondary feature-box-style-4">
+                                <div class="feature-box-icon">
+                                    <i class="icon-drop icons text-color-primary"></i>
+                                </div>
+                                <div class="feature-box-info">
+                                    <h4 class="mb-2 font-weight-bold">Colors</h4>
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing metus elit. Quisque rutrum pellentesque imperdiet.</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-4 " data-="fadeInRightShorter" data--delay="400">
+                            <div class="feature-box feature-box-secondary feature-box-style-4">
+                                <div class="feature-box-icon">
+                                    <i class="icon-mouse icons text-color-primary"></i>
+                                </div>
+                                <div class="feature-box-info">
+                                    <h4 class="mb-2 font-weight-bold">Buttons</h4>
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing metus elit. Quisque rutrum pellentesque imperdiet.</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+            <!-- Nuestros paquetes -->
+
+            <div class="container">
+                <div class="row pt-4 mt-5">
+                    <div class="col-lg-12 text-center appear-animation" data-appear-animation="fadeIn" data-appear-animation-delay="400">
+                        <h2 class="font-weight-normal text-6 mb-5">Our <strong class="font-weight-extra-bold">Prices</strong></h2>
+                    </div>
+                </div>
+
+                <div class="pricing-table pricing-table-no-gap mt-3 mb-5 pb-5">
+                    <div class="col-md-6 col-lg-3">
+                        <div class="plan">
+                            <div class="plan-header">
+                                <h3>Enterprise</h3>
+                            </div>
+                            <div class="plan-price">
+                                <span class="price"><span class="price-unit">$</span>59</span>
+                                <label class="price-label">PER MONTH</label>
+                            </div>
+                            <div class="plan-features">
+                                <ul>
+                                    <li>10GB Disk Space</li>
+                                    <li>100GB Monthly Bandwith</li>
+                                    <li>20 Email Accounts</li>
+                                    <li>Unlimited Subdomains</li>
+                                </ul>
+                            </div>
+                            <div class="plan-footer">
+                                <a href="#" class="btn btn-dark btn-modern btn-outline rounded-0 py-2 px-4">Choose Plan</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-6 col-lg-3">
+                        <div class="plan">
+                            <div class="plan-header">
+                                <h3>Professional</h3>
+                            </div>
+                            <div class="plan-price">
+                                <span class="price"><span class="price-unit">$</span>29</span>
+                                <label class="price-label">PER MONTH</label>
+                            </div>
+                            <div class="plan-features">
+                                <ul>
+                                    <li>5GB Disk Space</li>
+                                    <li>50GB Monthly Bandwith</li>
+                                    <li>10 Email Accounts</li>
+                                    <li>Unlimited Subdomains</li>
+                                </ul>
+                            </div>
+                            <div class="plan-footer">
+                                <a href="#" class="btn btn-dark btn-modern btn-outline rounded-0 py-2 px-4">Choose Plan</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-6 col-lg-3">
+                        <div class="plan plan-featured">
+                            <div class="plan-header bg-primary">
+                                <h3>Standard</h3>
+                            </div>
+                            <div class="plan-price">
+                                <span class="price"><span class="price-unit">$</span>17</span>
+                                <label class="price-label">PER MONTH</label>
+                            </div>
+                            <div class="plan-features">
+                                <ul>
+                                    <li>3GB Disk Space</li>
+                                    <li>25GB Monthly Bandwith</li>
+                                    <li>5 Email Accounts</li>
+                                    <li>Unlimited Subdomains</li>
+                                </ul>
+                            </div>
+                            <div class="plan-footer">
+                                <a href="#" class="btn btn-primary btn-modern rounded-0 py-2 px-4">Choose Plan</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-6 col-lg-3">
+                        <div class="plan">
+                            <div class="plan-header">
+                                <h3>Basic</h3>
+                            </div>
+                            <div class="plan-price">
+                                <span class="price"><span class="price-unit">$</span>9</span>
+                                <label class="price-label">PER MONTH</label>
+                            </div>
+                            <div class="plan-features">
+                                <ul>
+                                    <li>1GB Disk Space</li>
+                                    <li>10GB Monthly Bandwith</li>
+                                    <li>2 Email Accounts</li>
+                                    <li>Unlimited Subdomains</li>
+                                </ul>
+                            </div>
+                            <div class="plan-footer">
+                                <a href="#" class="btn btn-dark btn-modern btn-outline rounded-0 py-2 px-4">Choose Plan</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                
+            </div>
+
+
+
+<?php
+    include('sections/footer.php');
+?>
---
+++ sections/header.php
@@ -0,0 +1,181 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>High Marketing Studio | Diseño, Desarrollo web y Social Media</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    
+    <script src="main.js"></script>
+
+    <!-- Web Fonts  -->
+		<link href="/https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700,800%7CShadows+Into+Light%7CPlayfair+Display:400" rel="stylesheet" type="text/css">
+
+		<!-- Vendor CSS -->
+		<link rel="stylesheet" href="/vendor/bootstrap/css/bootstrap.min.css">
+		<link rel="stylesheet" href="/vendor/fontawesome-free/css/all.min.css">
+		<link rel="stylesheet" href="/vendor/animate/animate.min.css">
+		<link rel="stylesheet" href="/vendor/simple-line-icons/css/simple-line-icons.min.css">
+		<link rel="stylesheet" href="/vendor/owl.carousel/assets/owl.carousel.min.css">
+		<link rel="stylesheet" href="/vendor/owl.carousel/assets/owl.theme.default.min.css">
+		<link rel="stylesheet" href="/vendor/magnific-popup/magnific-popup.min.css">
+
+
+		<!-- Theme CSS -->
+		<link rel="stylesheet" href="/css/theme.css">
+		<link rel="stylesheet" href="/css/theme-elements.css">
+		<link rel="stylesheet" href="/css/theme-blog.css">
+		<link rel="stylesheet" href="/css/theme-shop.css">
+
+		<!-- Current Page CSS -->
+		<link rel="stylesheet" href="/vendor/rs-plugin/css/settings.css">
+		<link rel="stylesheet" href="/vendor/rs-plugin/css/layers.css">
+		<link rel="stylesheet" href="/vendor/rs-plugin/css/navigation.css">
+		
+		<!-- Demo CSS -->
+
+
+		<!-- Skin CSS -->
+		<link rel="stylesheet" href="/css/skins/skin-corporate-8.css"> 
+
+		<!-- Theme Custom CSS -->
+		<link rel="stylesheet" href="/css/custom.css">
+
+		<!-- Head Libs -->
+        <script src="vendor/modernizr/modernizr.min.js"></script>
+        <link rel="stylesheet" href="/css/css.css"/>
+</head>
+
+<body>
+    <div class="body">
+        <header id="header" data-plugin-options="{'stickyEnabled': true, 'stickyEnableOnBoxed': true, 'stickyEnableOnMobile': true, 'stickyStartAt': 164, 'stickySetTop': '-171px', 'stickyChangeLogo': false}">
+            <div class="header-body border-0">
+                <div class="header-top header-top-default border-bottom-0 bg-color-primary">
+                    <div class="container">
+                        <div class="header-row py-2">
+                            <div class="header-column justify-content-start">
+                                <div class="header-row">
+                                    <nav class="header-nav-top">
+                                        <!-- <ul class="nav nav-pills text-uppercase text-2">
+                                            <li class="nav-item nav-item-anim-icon">
+                                                <a class="nav-link pl-0 text-light opacity-7" href="/about-us.html"><i
+                                                        class="fas fa-angle-right"></i> About Us</a>
+                                            </li>
+                                            <li class="nav-item nav-item-anim-icon">
+                                                <a class="nav-link text-light opacity-7 pr-0" href="/contact-us.html"><i
+                                                        class="fas fa-angle-right"></i> Contact Us</a>
+                                            </li>
+                                        </ul> -->
+                                    </nav>
+                                </div>
+                            </div>
+                            <div class="header-column justify-content-end">
+                                <div class="header-row">
+                                    <ul class="header-social-icons social-icons d-none d-sm-block social-icons-clean social-icons-icon-light">
+                                        <li class="social-icons-facebook"><a href="/https://www.facebook.com/HighMKT" target="_blank"
+                                                title="Facebook"><i class="fab fa-facebook-f"></i></a></li>
+                                        <li class="social-icons-twitter"><a href="/https://twitter.com/@highmktstudio" target="_blank"
+                                                title="Twitter"><i class="fab fa-twitter"></i></a></li>
+                                        <li class="social-icons-linkedin"><a href="/https://www.linkedin.com/in/pdi-companies-b303149" target="_blank"
+                                                title="Linkedin"><i class="fab fa-linkedin-in"></i></a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="header-container container">
+                    <div class="header-row py-3">
+                        <div class="header-column justify-content-start w-50 order-md-1 d-none d-md-flex">
+                            <div class="header-row">
+                                <ul class="header-extra-info">
+                                    <li class="m-0">
+                                        <div class="feature-box feature-box-style-2 align-items-center">
+                                            <div class="feature-box-icon">
+                                                <i class="far fa-clock text-7 p-relative"></i>
+                                            </div>
+                                            <div class="feature-box-info">
+                                                <p class="pb-0 font-weight-semibold line-height-5 text-2">MON - FRI:
+                                                    10:00 - 18:00<br>SAT - SUN: 10:00 - 14:00</p>
+                                            </div>
+                                        </div>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div class="header-column justify-content-start justify-content-md-center order-1 order-md-2">
+                            <div class="header-row">
+                                <div class="header-logo">
+                                    <a href="/">
+                                        <img alt="HighLogo" width="150"  src="img/high-logo.png">
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="header-column justify-content-end w-50 order-2 order-md-3">
+                            <div class="header-row">
+                                <ul class="header-extra-info">
+                                    <li class="m-0">
+                                        <div class="feature-box reverse-allres feature-box-style-2 align-items-center">
+                                            <div class="feature-box-icon">
+                                                <i class="fab fa-whatsapp text-7 p-relative" style="top: -2px;"></i>
+                                            </div>
+                                            <div class="feature-box-info">
+                                                <p class="pb-0 font-weight-semibold line-height-5 text-2">(123)
+                                                    456-7890<br>(123) 456-7891</p>
+                                            </div>
+                                        </div>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="header-nav-bar header-nav-bar-top-border bg-light">
+                    <div class="header-container container">
+                        <div class="header-row">
+                            <div class="header-column">
+                                <div class="header-row justify-content-end">
+                                    <div class="header-nav p-0">
+                                        <div class="header-nav header-nav-line header-nav-divisor header-nav-spaced justify-content-lg-center">
+                                            <div class="header-nav-main header-nav-main-square header-nav-main-effect-1 header-nav-main-sub-effect-1">
+                                                <nav class="collapse">
+                                                    <ul class="nav nav-pills flex-column flex-lg-row" id="mainNav">
+                                                        <li class="dropdown">
+                                                            <a class="dropdown-item dropdown-toggle active" href="/">
+                                                                Inicio
+                                                            </a>
+                                                        </li>
+                                                        <li class="dropdown">
+                                                            <a class="dropdown-item dropdown-toggle" href="/#">
+                                                                Servicios
+                                                            </a>
+                                                        </li>
+                                                        <li class="dropdown">
+                                                            <a class="dropdown-item dropdown-toggle" href="/#">
+                                                                Portafolio
+                                                            </a>
+                                                        </li>
+                                                        <li class="dropdown">
+                                                            <a class="dropdown-item dropdown-toggle" href="/#">
+                                                                Contacto
+                                                            </a>
+                                                        </li>
+                                                    </ul>
+                                                </nav>
+                                            </div>
+                                            <button class="btn header-btn-collapse-nav" data-toggle="collapse"
+                                                data-target=".header-nav-main nav">
+                                                <i class="fas fa-bars"></i>
+                                            </button>
+                                        </div>
+                                        <a class="linklenguage" href="/#">English</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </header>
