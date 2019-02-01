cb6ffba3e3a3e7cc41afbec7912e875dd3611789
Armando <armando567@hotmail.com>
2019-02-01 14:00:40 -0600

    Diseño Json
    Se diseño la estructura del Json para imprimir los textos en la pagina de servicios

--- service.php
+++ service.php
@@ -1,13 +1,12 @@
 <?php
+service();
 function service(){
 $data = file_get_contents("json/diseño_web.json");
 $servicios = json_decode($data, true);
 
-//print_r($servicios['services']);
 foreach ($servicios['Design_web'] as $service) {
   $result=$service['es'];
-
-  }
+}
   return $result;
 }
  ?>
